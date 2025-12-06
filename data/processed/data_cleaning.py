d428e0b07ada63d58f37ddb83cf479d526a15fb1
chopin0807@naver.com <junho08075@gmail.com>
2025-12-06 23:44:59 +0900

    fix: 전처리 코드 수정(표 데이터 보존을 위한 띄어쓰기 전처리 제외)

---
+++ data/processed/data_cleaning.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+keyword = "Cell"
+df = pd.read_csv(f"api_data_{keyword}.csv")
+# url 중복제거 후 테이블
+df = df.drop_duplicates(subset="url", keep="first").reset_index()
+df = df.rename(columns={'index': 'protocol_id'})
+
+# title 중복제거 후 테이블
+df = df.drop_duplicates(subset="title", keep="first").reset_index()
+df = df.drop(columns=['index'])
+
+df = df.replace(r'"+', '"', regex=True)
+df = df.replace(r'\n+', '\n', regex=True)
+
+df = df.replace("<no data>", "")
+df = df.replace(r"\\u[0-9a-fA-F]{4}", "", regex=True)
+
+# 따옴표만 있거나 공백만 있는 경우 NaN으로 변환
+df = df.replace(r'^\s*["“”\'’]*\s*$', "", regex=True)
+df = df.replace("NA", "")
+
+# NaN을 빈 문자열로 변경
+df = df.fillna("")
+
+# 공백만 있는 문자열을 빈 문자열로 변경
+df = df.applymap(lambda x: "" if isinstance(x, str) and x.strip() == "" else x)
+df.to_csv(f"cleaned_data_{keyword}.csv", index=False)
