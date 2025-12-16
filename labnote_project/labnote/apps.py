9625d5a4f82a33e3a7a7d5193603188ad2f0b6a3
chopin0807@naver.com <junho08075@gmail.com>
2025-12-16 09:13:56 +0900

    remove: labnote 관련 제거

--- labnote_project/labnote/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LabnoteConfig(AppConfig):
-    name = 'labnote'
--- labnote_project/migrate.cmd
+++
@@ -1,2 +0,0 @@
-python manage.py makemigrations accounts notes folders bookmarks attachments
-python manage.py migrate
