2b94fe48e7c6405a16facd3f22228ecccd7a2d86
chopin0807@naver.com <junho08075@gmail.com>
2025-12-31 09:44:09 +0900

    fix: uniprot api이용시 로그인 필수 반영하여 원복

--- django_app/apps/experiments/views.py
+++ django_app/apps/experiments/views.py
@@ -195,7 +195,7 @@ def index(request):
     responses={200: dict},
 )
 @api_view(['GET', 'POST'])
-@permission_classes([])
+@permission_classes([IsAuthenticated])
 def uniprot_search_api(request):
     # 1. Query parameter 수집
     keyword = request.query_params.get("keyword")
